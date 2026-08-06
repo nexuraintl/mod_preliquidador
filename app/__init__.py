033798f66dbb55a9773f214ba50b7042aeb17448
Fredis Bobadillo <fbobadillo@nexura.com>
2026-08-06 15:11:18 +0000

    Updated __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,24 @@
 from .validacion_negocios import validar_negocio
 from .validacion_archivos import ValidadorArchivos,ResultadoValidacionArchivos
+from .descarga_archivos import (
+    ArchivoInvalido,
+    DescargaAbortada,
+    DescargadorArchivos,
+    ResultadoDescarga,
+    nombre_seguro,
+    validar_adjunto,
+    validar_adjuntos,
+)
 
 __all__ = [
     'validar_negocio',
     'ValidadorArchivos',
-    'ResultadoValidacionArchivos'
+    'ResultadoValidacionArchivos',
+    'ArchivoInvalido',
+    'DescargaAbortada',
+    'DescargadorArchivos',
+    'ResultadoDescarga',
+    'nombre_seguro',
+    'validar_adjunto',
+    'validar_adjuntos'
 ]
